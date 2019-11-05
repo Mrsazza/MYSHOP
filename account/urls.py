dcce616d9cff7546eafef0372e50a9073d5728cc
shahriar <shahriarhossain147@gmail.com>
2019-11-05 18:39:00 +0600

    password change and resetting password added

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='passwod_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
