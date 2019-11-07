9cd5efc775baa06203adc794959612fa9038347e
Mrsazza <shahriarhossain147@gmail.com>
2019-11-07 08:31:54 +0600

    user sign up added

--- account/views.py
+++ account/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user objectt but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # set chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # save the user object
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                    'account/register.html',
+                    {'user_form': user_form})
 
 def user_login(request):
     if request.method == 'POST':
@@ -23,8 +45,13 @@ def user_login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html',{'form':form})
+    return render(request, 'account/login.html', {'form': form})
+
+
 
 @login_required
 def home(request):
-    return render(request,'shop/product/list.html', {'section': 'home'})
+    return render(request, 'shop/product/list.html', {'section': 'home'})
+
+
+
