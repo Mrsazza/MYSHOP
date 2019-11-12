b8054369c366b97c81ca06b61859b13e7d24b3a6
shahriar <shahriarhossain147@gmail.com>
2019-11-12 08:12:00 +0600

    order model added

--- account/models.py
+++ account/models.py
@@ -5,9 +5,9 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to = 'users/%Y/%m/%d',
-                              blank=True)
+    mobile_number = models.CharField(max_length = 11, blank = True)
+    address = models.TextField(blank=True)
+    city = models.CharField(max_length =100,blank=True)
     
     def __str__(self):
         return 'Profile For user {}'.format(self.user.username)
